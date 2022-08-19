c6b49b695c22a43ea99686520fd5df1386e512bd
danielrubens <danielrubensdaniel@gmail.com>
2022-08-19 16:16:01 -0300

    01

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Farewell, front-end', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testa o componente App', () => {
+  test('Farewell, front-end', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/TRYBE/i);
+    expect(linkElement).toBeInTheDocument();
+  });  
+})
+
