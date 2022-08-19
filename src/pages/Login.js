511cf885cdb8fbff0552c68fb42f02ec5a893ffb
danielrubens <danielrubensdaniel@gmail.com>
2022-08-19 19:18:01 -0300

    04 05 06 Por finalizar. history.push(/foods) sem ser reconhecido nos teste

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import emailAction from '../redux/actions';
 
-function Login() {
+const valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const magicNumber = 6;
+
+function Login({ dispatchEmail }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [button, setButton] = useState(true);
+  const history = useHistory();
+
+  const sendToLocalStorage = () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+
+    history.push('/foods');
+  };
+
   return (
     <div>
       <input
         type="email"
-        placehold="Email"
+        placeholder="Email"
         data-testid="email-input"
+        onChange={ ({ target }) => {
+          setEmail(target.value);
+          setButton(!(valid.test(target.value) && password.length > magicNumber));
+        } }
       />
       <input
         type="password"
         placeholder="Password"
         data-testid="password-input"
+        onChange={ ({ target }) => {
+          setPassword(target.value);
+          setButton(!(valid.test(email) && target.value.length > magicNumber));
+        } }
       />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ button }
+        onClick={ () => {
+          dispatchEmail(email);
+          sendToLocalStorage();
+        } }
       >
         Enter
       </button>
@@ -24,4 +57,10 @@ function Login() {
   );
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchEmail: (payload) => dispatch(emailAction(payload)),
+});
+
+Login.propTypes = { dispatchEmail: PropTypes.func.isRequired };
+
+export default connect(null, mapDispatchToProps)(Login);
