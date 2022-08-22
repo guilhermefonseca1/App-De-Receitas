9bd5c5389943381764b0b8ef49a855ec10ec5a5a
Carla Uyemura <carla.uyemura@gmail.com>
2022-08-22 13:56:48 -0300

    Incluindo ajuste css header e body

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ page, search }) {
-  // const usePathname = () => {
-  //   const location = useLocation();
-  //   return setTitle(location.pathname.split('/')[1].toUpperCase());
-  // };
+  const history = useHistory();
 
   return (
     <header>
-      <button type="button">
+      <button type="button" onClick={ () => history.push('/profile') }>
         <img data-testid="profile-top-btn" src={ profileIcon } alt="icon-profile" />
       </button>
       <h1 data-testid="page-title" className="title-page">{page}</h1>
@@ -19,7 +17,7 @@ function Header({ page, search }) {
         search
 
             && (
-              <button type="button">
+              <button type="button" onClick={ () => {} }>
                 <img data-testid="search-top-btn" src={ searchIcon } alt="icon-search" />
               </button>
             )
