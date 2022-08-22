7ff807c0401c63f524b222fc04ef851d49b69d16
Lais <laismassacesi@gmail.com>
2022-08-22 15:10:32 -0300

    Requisito 9

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header({ page, search }) {
   // const usePathname = () => {
   //   const location = useLocation();
   //   return setTitle(location.pathname.split('/')[1].toUpperCase());
   // };
+  const [isToggle, setToggle] = useState(false);
 
   return (
     <header>
@@ -18,12 +20,13 @@ function Header({ page, search }) {
       {
         search
 
-            && (
-              <button type="button">
-                <img data-testid="search-top-btn" src={ searchIcon } alt="icon-search" />
-              </button>
-            )
+          && (
+            <button type="button" onClick={ () => setToggle(!isToggle) }>
+              <img data-testid="search-top-btn" src={ searchIcon } alt="icon-search" />
+            </button>
+          )
       }
+      { isToggle && <SearchBar />}
     </header>
   );
 }
