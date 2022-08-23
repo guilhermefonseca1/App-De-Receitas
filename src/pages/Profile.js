25fb2b65eb9e7662eaeea41f1c5ca401885628d9
Lais <laismassacesi@gmail.com>
2022-08-23 17:35:35 -0300

    Requisitos 57, 58 e 59

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,11 +3,19 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const userObj = window.localStorage.getItem('user');
+  const obj = JSON.parse(userObj);
+  const userEmail = obj.email;
   return (
     <div>
       <Header page="Profile" search={ false } />
+      <p data-testid="profile-email">{userEmail}</p>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
       <Footer />
     </div>
   );
 }
+
 export default Profile;
