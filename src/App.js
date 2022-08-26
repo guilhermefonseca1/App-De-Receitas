ac5f732ec2ed833cc46d61e98ac74d4a87b5a67b
Matheus Bora <matheusmcb@gmail.com>
2022-08-26 15:01:55 -0300

    commit para pull

--- src/App.js
+++ src/App.js
@@ -15,13 +15,10 @@ function App() {
     <div className="meals">
       <Switch>
         <Route exact path="/" component={ Login } />
-        {/* <<<<<<< HEAD */}
         <Route exact path="/foods" component={ Foods } />
         <Route exact path="/foods/:id" component={ RecipeDetails } />
-        {/* ======= */}
         <Route path="/foods" exact component={ Foods } />
         <Route exact path="/foods/:id" component={ RecipeDetails } />
-        {/* >>>>>>> 195a2603cb651c9bae8687cd42d75a004cba4dd7 */}
         <Route
           path="/foods/:id/in-progress"
           component={ RecipeInProgress }
