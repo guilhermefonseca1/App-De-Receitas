a2d840a5c4d36d7abb9ab7fd8b77f12da53e6b8f
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-08-30 19:16:06 -0300

    red

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -16,6 +16,26 @@ function FavoriteRecipes() {
       image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg' }];
     setListRecipes(searchItem);
   }, []);
+
+  const [filterRecipes, setFilterRecipes] = useState([]);
+
+  const doneRecipes = () => {
+    if (localStorage.getItem('doneRecipes')) {
+      setFilterRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
+    }
+  };
+
+  useEffect(() => {
+    doneRecipes();
+  }, []);
+  const handleFilterRecipes = ({ target }) => {
+    const { name } = target;
+    doneRecipes();
+    if (name !== 'all') {
+      const newRecipes = filterRecipes.filter((item) => item.type === name);
+      setFilterRecipes(newRecipes);
+    }
+  };
   return (
     <div>
       <Header page="Favorite Recipes" search={ false } />
@@ -24,6 +44,7 @@ function FavoriteRecipes() {
           type="button"
           data-testid="filter-by-all-btn"
           name="all"
+          onClick={ (element) => handleFilterRecipes(element) }
         >
           All
         </button>
@@ -31,7 +52,7 @@ function FavoriteRecipes() {
           type="button"
           data-testid="filter-by-food-btn"
           name="food"
-          // onClick={ (element) => handleFilterRecipes(element) }
+          onClick={ (element) => handleFilterRecipes(element) }
         >
           Foods
         </button>
@@ -39,7 +60,7 @@ function FavoriteRecipes() {
           type="button"
           data-testid="filter-by-drink-btn"
           name="drink"
-          // onClick={ (element) => handleFilterRecipes(element) }
+          onClick={ (element) => handleFilterRecipes(element) }
         >
           Drinks
         </button>
