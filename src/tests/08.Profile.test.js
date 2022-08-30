d51820bcf2426226752b61cbaf2285959a123031
danielrubens <danielrubensdaniel@gmail.com>
2022-08-30 14:42:43 -0300

    60. Teste com 45% de cobertura

--- src/tests/08.Profile.test.js
+++ src/tests/08.Profile.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import Profile from '../pages/Profile';
+import { screen} from '@testing-library/react';
 
 describe('Tests the Profile component', () => {
   it('checks if the text is rendered while typing', () => {
-    const {getByTestId} = renderWithRouterAndRedux(<Profile />)
-    ['profile-email', 'profile-done-btn','profile-logout-btn','profile-favorite-btn'].
-    forEach((i) => expect(getByTestId(i).toBeDefined()));
+    renderWithRouterAndRedux(<Profile />)
+    const profile = screen.getByTestId('profile-email')
+    expect(profile).toBeDefined()
+    const done = screen.getByTestId('profile-done-btn')
+    const favorite = screen.getByTestId('profile-favorite-btn')
+    const logout = screen.getByTestId('profile-logout-btn')
+    userEvent.click(done)
+    userEvent.click(favorite)
+    userEvent.click(logout)
   })
 });
